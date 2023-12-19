89cc91310d3a87fa078402c5e526735e70348531
ericchase <github@eric.addymail.com>
2023-12-19 01:32:26 -0600

    update language and examples
    Signed-off-by: ericchase <github@eric.addymail.com>

--- build-deno.ps1
+++ build-deno.ps1
@@ -3,4 +3,4 @@ if ((Get-Command "deno.exe" -ErrorAction SilentlyContinue) -eq $null)
   irm https://deno.land/install.ps1 | iex
 }
 
-deno compile --allow-read --output doto.exe ./src/main.mjs
+deno compile --allow-read --allow-run --output doto.exe ./src/main.mjs
--- test.bat
+++ test.bat
@@ -1,7 +1,1 @@
-pushd example
-..\doto help
-..\doto version
-..\doto parse build
-..\doto check build
-..\doto run "echo hello world"
-popd
+.\doto.exe build
