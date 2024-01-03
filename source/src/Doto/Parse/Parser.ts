445b7f1a22f96c4d678745d9c4168a5c34b86d2f
ericchase <github@eric.addymail.com>
2024-01-03 05:05:11 -0600

    normalized public interface for parser
    Signed-off-by: ericchase <github@eric.addymail.com>

--- source/src/Doto/Parse/Parser.ts
+++ source/src/Doto/Parse/Parser.ts
@@ -1,51 +1,8 @@
 import { Uint8Reader, Uint8Type, Uint8View } from '../Uint8.js';
 import { IToken, Token, TokenType, Uint8ToTokenType } from './Token.js';
 
-export interface IParsedLine {
-  lineNumber: number;
-  tokens: IToken[];
-}
-
-export class ParsedLine implements IParsedLine {
-  constructor(
-    public lineNumber: number,
-    public tokens: IToken[],
-  ) {}
-}
-
-export async function TokenizeLine(buffer: Uint8Array): Promise<IToken[]> {
-  const tokens: IToken[] = [];
-
-  for (let i = 0; i < buffer.byteLength; i++) {
-    const tokenType = Uint8ToTokenType(buffer[i]);
-
-    switch (tokenType) {
-      case TokenType.BackSlash:
-      case TokenType.DoubleQuote:
-        tokens.push(new Token(tokenType, new Uint8View(buffer, i, i + 1)));
-        break;
-      case TokenType.Glyph:
-      case TokenType.WhiteSpace:
-        {
-          const start = i;
-          for (; i < buffer.byteLength; i++) {
-            if (i + 1 === buffer.byteLength || Uint8ToTokenType(buffer[i + 1]) !== tokenType) {
-              tokens.push(new Token(tokenType, new Uint8View(buffer, start, i + 1)));
-              break;
-            }
-          }
-        }
-        break;
-    }
-  }
-
-  return tokens;
-}
-
-export async function Tokenize(reader: Uint8Reader): Promise<IParsedLine[]> {
-  const parsedLines: IParsedLine[] = [];
+export async function ApplyToLines(reader: Uint8Reader, fn: (lineNumber: number, lineBuffer: Uint8Array) => Promise<void>): Promise<void> {
   let lineNumber = 1;
-
   const buffer = new Uint8Array(65536);
   let bufferLength = 0;
   while (true) {
@@ -69,7 +26,7 @@ export async function Tokenize(reader: Uint8Reader): Promise<IParsedLine[]> {
       if (buffer[lineEnd] === Uint8Type.LF) {
         if (lineEnd - lineStart > 0) {
           const lineBuffer = new Uint8View(buffer, lineStart, lineEnd).toBytes();
-          parsedLines.push(new ParsedLine(lineNumber, await TokenizeLine(lineBuffer)));
+          await fn(lineNumber, lineBuffer);
         }
         lineStart = lineEnd + 1;
         lineNumber++;
@@ -78,10 +35,84 @@ export async function Tokenize(reader: Uint8Reader): Promise<IParsedLine[]> {
     buffer.copyWithin(0, lineStart, lineEnd);
     bufferLength = lineEnd - lineStart;
   }
-
-  return parsedLines;
 }
 
-async function Parse(reader: Uint8Reader) {
-  //
+export interface ITokenizedLine {
+  number: number;
+  tokens: IToken[];
+}
+
+export class TokenizedLine implements ITokenizedLine {
+  constructor(
+    public number: number,
+    public tokens: IToken[],
+  ) {}
+}
+
+export function TokenizeLine(lineNumber: number, lineBuffer: Uint8Array): ITokenizedLine {
+  const tokens: IToken[] = [];
+
+  for (let i = 0; i < lineBuffer.byteLength; i++) {
+    const tokenType = Uint8ToTokenType(lineBuffer[i]);
+
+    switch (tokenType) {
+      case TokenType.BackSlash:
+      case TokenType.DoubleQuote:
+        tokens.push(new Token(tokenType, new Uint8View(lineBuffer, i, i + 1)));
+        break;
+      case TokenType.Glyph:
+      case TokenType.WhiteSpace:
+        {
+          const start = i;
+          for (; i < lineBuffer.byteLength; i++) {
+            if (i + 1 === lineBuffer.byteLength || Uint8ToTokenType(lineBuffer[i + 1]) !== tokenType) {
+              tokens.push(new Token(tokenType, new Uint8View(lineBuffer, start, i + 1)));
+              break;
+            }
+          }
+        }
+        break;
+    }
+  }
+
+  return new TokenizedLine(lineNumber, tokens);
+}
+
+export async function Tokenize(reader: Uint8Reader): Promise<ITokenizedLine[]> {
+  const lines: ITokenizedLine[] = [];
+  await ApplyToLines(reader, async (lineNumber: number, lineBuffer: Uint8Array) => {
+    const tokenizedLine = TokenizeLine(lineNumber, lineBuffer);
+    if (tokenizedLine.tokens.length > 0) {
+      lines.push(tokenizedLine);
+    }
+  });
+  return lines;
+}
+
+export interface IParsedLine {
+  number: number;
+  tokens: IToken[];
+}
+
+export class ParsedLine implements IParsedLine {
+  constructor(
+    public number: number,
+    public tokens: IToken[],
+  ) {}
+}
+
+export function ParseLine(line: ITokenizedLine): IParsedLine {
+  // TODO:
+  return new ParsedLine(line.number, line.tokens);
+}
+
+export async function Parse(reader: Uint8Reader): Promise<IParsedLine[]> {
+  const lines: IParsedLine[] = [];
+  await ApplyToLines(reader, async (lineNumber: number, lineBuffer: Uint8Array) => {
+    const parsedLine = ParseLine(TokenizeLine(lineNumber, lineBuffer));
+    if (parsedLine.tokens.length > 0) {
+      lines.push(parsedLine);
+    }
+  });
+  return lines;
 }
--- test/src/Doto/Parse/Parser.test.ts
+++ test/src/Doto/Parse/Parser.test.ts
@@ -23,49 +23,49 @@ function checkToken(token: Token, type: TokenType, text: string) {
 describe('Parser', () => {
   describe('TokenizeLine', () => {
     test('empty', async () => {
-      const tokens = await TokenizeLine(EmptyUint8Array);
-      expect(tokens.length).toBe(0);
+      const line = TokenizeLine(1, EmptyUint8Array);
+      expect(line.tokens.length).toBe(0);
     });
     test('1 glyph', async () => {
-      const tokens = await TokenizeLine(encode('glyph'));
-      expect(tokens.length).toBe(1);
-      expect(checkToken(tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
+      const line = TokenizeLine(1, encode('glyph'));
+      expect(line.tokens.length).toBe(1);
+      expect(checkToken(line.tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
     });
     test('2 glyphs', async () => {
-      const tokens = await TokenizeLine(encode('glyph glyph'));
-      expect(tokens.length).toBe(3);
-      expect(checkToken(tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
-      expect(checkToken(tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
-      expect(checkToken(tokens[2], TokenType.Glyph, 'glyph')).toBe(true);
+      const line = TokenizeLine(1, encode('glyph glyph'));
+      expect(line.tokens.length).toBe(3);
+      expect(checkToken(line.tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
+      expect(checkToken(line.tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
+      expect(checkToken(line.tokens[2], TokenType.Glyph, 'glyph')).toBe(true);
     });
     test('double quotes', async () => {
-      const tokens = await TokenizeLine(encode('"glyph"'));
-      expect(tokens.length).toBe(3);
-      expect(checkToken(tokens[0], TokenType.DoubleQuote, '"')).toBe(true);
-      expect(checkToken(tokens[1], TokenType.Glyph, 'glyph')).toBe(true);
-      expect(checkToken(tokens[2], TokenType.DoubleQuote, '"')).toBe(true);
+      const line = TokenizeLine(1, encode('"glyph"'));
+      expect(line.tokens.length).toBe(3);
+      expect(checkToken(line.tokens[0], TokenType.DoubleQuote, '"')).toBe(true);
+      expect(checkToken(line.tokens[1], TokenType.Glyph, 'glyph')).toBe(true);
+      expect(checkToken(line.tokens[2], TokenType.DoubleQuote, '"')).toBe(true);
     });
     test('backslashes', async () => {
-      const tokens = await TokenizeLine(encode('a\\b\\c'));
-      expect(tokens.length).toBe(5);
-      expect(checkToken(tokens[0], TokenType.Glyph, 'a')).toBe(true);
-      expect(checkToken(tokens[1], TokenType.BackSlash, '\\')).toBe(true);
-      expect(checkToken(tokens[2], TokenType.Glyph, 'b')).toBe(true);
-      expect(checkToken(tokens[3], TokenType.BackSlash, '\\')).toBe(true);
-      expect(checkToken(tokens[4], TokenType.Glyph, 'c')).toBe(true);
+      const line = TokenizeLine(1, encode('a\\b\\c'));
+      expect(line.tokens.length).toBe(5);
+      expect(checkToken(line.tokens[0], TokenType.Glyph, 'a')).toBe(true);
+      expect(checkToken(line.tokens[1], TokenType.BackSlash, '\\')).toBe(true);
+      expect(checkToken(line.tokens[2], TokenType.Glyph, 'b')).toBe(true);
+      expect(checkToken(line.tokens[3], TokenType.BackSlash, '\\')).toBe(true);
+      expect(checkToken(line.tokens[4], TokenType.Glyph, 'c')).toBe(true);
     });
     test('backslashes and double quotes', async () => {
-      const tokens = await TokenizeLine(encode('"a\\"b\\"c"'));
-      expect(tokens.length).toBe(9);
-      expect(checkToken(tokens[0], TokenType.DoubleQuote, '"')).toBe(true);
-      expect(checkToken(tokens[1], TokenType.Glyph, 'a')).toBe(true);
-      expect(checkToken(tokens[2], TokenType.BackSlash, '\\')).toBe(true);
-      expect(checkToken(tokens[3], TokenType.DoubleQuote, '"')).toBe(true);
-      expect(checkToken(tokens[4], TokenType.Glyph, 'b')).toBe(true);
-      expect(checkToken(tokens[5], TokenType.BackSlash, '\\')).toBe(true);
-      expect(checkToken(tokens[6], TokenType.DoubleQuote, '"')).toBe(true);
-      expect(checkToken(tokens[7], TokenType.Glyph, 'c')).toBe(true);
-      expect(checkToken(tokens[8], TokenType.DoubleQuote, '"')).toBe(true);
+      const line = TokenizeLine(1, encode('"a\\"b\\"c"'));
+      expect(line.tokens.length).toBe(9);
+      expect(checkToken(line.tokens[0], TokenType.DoubleQuote, '"')).toBe(true);
+      expect(checkToken(line.tokens[1], TokenType.Glyph, 'a')).toBe(true);
+      expect(checkToken(line.tokens[2], TokenType.BackSlash, '\\')).toBe(true);
+      expect(checkToken(line.tokens[3], TokenType.DoubleQuote, '"')).toBe(true);
+      expect(checkToken(line.tokens[4], TokenType.Glyph, 'b')).toBe(true);
+      expect(checkToken(line.tokens[5], TokenType.BackSlash, '\\')).toBe(true);
+      expect(checkToken(line.tokens[6], TokenType.DoubleQuote, '"')).toBe(true);
+      expect(checkToken(line.tokens[7], TokenType.Glyph, 'c')).toBe(true);
+      expect(checkToken(line.tokens[8], TokenType.DoubleQuote, '"')).toBe(true);
     });
   });
   describe('Tokenize', () => {
@@ -80,6 +80,7 @@ describe('Parser', () => {
     test('1 glyph', async () => {
       const lines = await Tokenize(new StringReader('glyph'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       let i = 0;
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
@@ -87,6 +88,7 @@ describe('Parser', () => {
     test('1 glyph, begins with linefeed', async () => {
       const lines = await Tokenize(new StringReader('\nglyph'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(2);
       expect(lines[0].tokens.length).toBe(1);
       let i = 0;
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
@@ -94,6 +96,7 @@ describe('Parser', () => {
     test('1 glyph, ends with linefeed', async () => {
       const lines = await Tokenize(new StringReader('glyph\n'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       let i = 0;
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
@@ -101,6 +104,7 @@ describe('Parser', () => {
     test('1 glyph, begins and ends with linefeed', async () => {
       const lines = await Tokenize(new StringReader('\nglyph\n'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(2);
       expect(lines[0].tokens.length).toBe(1);
       let i = 0;
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
@@ -108,6 +112,7 @@ describe('Parser', () => {
     test('2 glyphs, 1 line', async () => {
       const lines = await Tokenize(new StringReader('glyph glyph'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(3);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
       expect(checkToken(lines[0].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
@@ -116,6 +121,7 @@ describe('Parser', () => {
     test('3 glyphs, 1 line', async () => {
       const lines = await Tokenize(new StringReader('one two three'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(5);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'one')).toBe(true);
       expect(checkToken(lines[0].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
@@ -126,6 +132,7 @@ describe('Parser', () => {
     test('3 glyphs, 1 line, begins and ends with linefeed', async () => {
       const lines = await Tokenize(new StringReader('\none two three\n'));
       expect(lines.length).toBe(1);
+      expect(lines[0].number).toBe(2);
       expect(lines[0].tokens.length).toBe(5);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'one')).toBe(true);
       expect(checkToken(lines[0].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
@@ -136,46 +143,58 @@ describe('Parser', () => {
     test('1 glyph x 2 lines', async () => {
       const lines = await Tokenize(new StringReader('glyph\nglyph'));
       expect(lines.length).toBe(2);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
+      expect(lines[1].number).toBe(2);
       expect(lines[1].tokens.length).toBe(1);
       expect(checkToken(lines[1].tokens[0], TokenType.Glyph, 'glyph')).toBe(true);
     });
     test('1 glyph x 3 lines', async () => {
       const lines = await Tokenize(new StringReader('one\ntwo\nthree'));
       expect(lines.length).toBe(3);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'one')).toBe(true);
+      expect(lines[1].number).toBe(2);
       expect(lines[1].tokens.length).toBe(1);
       expect(checkToken(lines[1].tokens[0], TokenType.Glyph, 'two')).toBe(true);
+      expect(lines[2].number).toBe(3);
       expect(lines[2].tokens.length).toBe(1);
       expect(checkToken(lines[2].tokens[0], TokenType.Glyph, 'three')).toBe(true);
     });
     test('1 glyph x 3 lines, begins and ends with linefeed', async () => {
       const lines = await Tokenize(new StringReader('\none\ntwo\nthree\n'));
       expect(lines.length).toBe(3);
+      expect(lines[0].number).toBe(2);
       expect(lines[0].tokens.length).toBe(1);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'one')).toBe(true);
+      expect(lines[1].number).toBe(3);
       expect(lines[1].tokens.length).toBe(1);
       expect(checkToken(lines[1].tokens[0], TokenType.Glyph, 'two')).toBe(true);
+      expect(lines[2].number).toBe(4);
       expect(lines[2].tokens.length).toBe(1);
       expect(checkToken(lines[2].tokens[0], TokenType.Glyph, 'three')).toBe(true);
     });
     test('1 space x 2 lines', async () => {
       const lines = await Tokenize(new StringReader(' \n '));
       expect(lines.length).toBe(2);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       expect(checkToken(lines[0].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
+      expect(lines[1].number).toBe(2);
       expect(lines[1].tokens.length).toBe(1);
       expect(checkToken(lines[1].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
     });
     test('2 glyphs x 2 lines', async () => {
       const lines = await Tokenize(new StringReader('one two\nthree four'));
       expect(lines.length).toBe(2);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(3);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'one')).toBe(true);
       expect(checkToken(lines[0].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
       expect(checkToken(lines[0].tokens[2], TokenType.Glyph, 'two')).toBe(true);
+      expect(lines[1].number).toBe(2);
       expect(lines[1].tokens.length).toBe(3);
       expect(checkToken(lines[1].tokens[0], TokenType.Glyph, 'three')).toBe(true);
       expect(checkToken(lines[1].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
@@ -184,26 +203,34 @@ describe('Parser', () => {
     test('mix and match', async () => {
       const lines = await Tokenize(new StringReader('a\n lazy \ndog\nand \na quick\n fox\n \n \n'));
       expect(lines.length).toBe(8);
+      expect(lines[0].number).toBe(1);
       expect(lines[0].tokens.length).toBe(1);
       expect(checkToken(lines[0].tokens[0], TokenType.Glyph, 'a')).toBe(true);
+      expect(lines[1].number).toBe(2);
       expect(lines[1].tokens.length).toBe(3);
       expect(checkToken(lines[1].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
       expect(checkToken(lines[1].tokens[1], TokenType.Glyph, 'lazy')).toBe(true);
       expect(checkToken(lines[1].tokens[2], TokenType.WhiteSpace, ' ')).toBe(true);
+      expect(lines[2].number).toBe(3);
       expect(lines[2].tokens.length).toBe(1);
       expect(checkToken(lines[2].tokens[0], TokenType.Glyph, 'dog')).toBe(true);
+      expect(lines[3].number).toBe(4);
       expect(lines[3].tokens.length).toBe(2);
       expect(checkToken(lines[3].tokens[0], TokenType.Glyph, 'and')).toBe(true);
       expect(checkToken(lines[3].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
+      expect(lines[4].number).toBe(5);
       expect(lines[4].tokens.length).toBe(3);
       expect(checkToken(lines[4].tokens[0], TokenType.Glyph, 'a')).toBe(true);
       expect(checkToken(lines[4].tokens[1], TokenType.WhiteSpace, ' ')).toBe(true);
       expect(checkToken(lines[4].tokens[2], TokenType.Glyph, 'quick')).toBe(true);
+      expect(lines[5].number).toBe(6);
       expect(lines[5].tokens.length).toBe(2);
       expect(checkToken(lines[5].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
       expect(checkToken(lines[5].tokens[1], TokenType.Glyph, 'fox')).toBe(true);
+      expect(lines[6].number).toBe(7);
       expect(lines[6].tokens.length).toBe(1);
       expect(checkToken(lines[6].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
+      expect(lines[7].number).toBe(8);
       expect(lines[7].tokens.length).toBe(1);
       expect(checkToken(lines[7].tokens[0], TokenType.WhiteSpace, ' ')).toBe(true);
     });
